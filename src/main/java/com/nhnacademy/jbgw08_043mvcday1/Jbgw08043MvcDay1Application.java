bd2a18dfb9cd9fd97126876df97bc69729dd0236
조승주 <tmdwn3318@gmail.com>
2024-11-11 17:37:13 +0900

    init

---
+++ src/main/java/com/nhnacademy/jbgw08_043mvcday1/Jbgw08043MvcDay1Application.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.jbgw08_043mvcday1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jbgw08043MvcDay1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jbgw08043MvcDay1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/nhnacademy/jbgw08_043mvcday1/Jbgw08043MvcDay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.jbgw08_043mvcday1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jbgw08043MvcDay1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
