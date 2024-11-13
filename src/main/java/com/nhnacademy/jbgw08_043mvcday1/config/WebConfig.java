3565929c711494ca82be6cffb37a1255706e9e0b
조승주 <tmdwn3318@gmail.com>
2024-11-13 16:30:37 +0900

    webconfig에 로그인체크 인터셉트 추가하고 제외 경로 지정 로그인체크 인터셉트 만들어만 놓음 preHandle 구현 필요

--- src/main/java/com/nhnacademy/jbgw08_043mvcday1/config/WebConfig.java
+++ src/main/java/com/nhnacademy/jbgw08_043mvcday1/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.jbgw08_043mvcday1.config;
 
+import com.nhnacademy.jbgw08_043mvcday1.interceptor.LoginCheckInterceptor;
 import org.apache.logging.log4j.message.Message;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -37,6 +38,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
+
+        // 로그인 체크 인터셉터를 추가
+        // 체크 제외해야 하는 경로
+        // /student/register
+        // /student/login
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .excludePathPatterns("/student/register", "student/login");
     }
 
 }
